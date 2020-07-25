c402b24696c61f9fb8c2b2cc2408364b75a69cec
timoe2020 <64744101+timoe2020@users.noreply.github.com>
2020-07-25 16:35:16 +0800

    Update delete.php

--- admin/shopclass/delete.php
+++ admin/shopclass/delete.php
@@ -1,1 +1,13 @@
 <?php
+include'../../public/common/dbconfig.inc.php';
+$id=$_GET['id'];
+$sql="DELETE FROM shopclass WHERE id='{$id}'";
+if($conn->query($sql))
+{
+    echo "删除成功！";
+    echo"<a href='index.php'>点击返回商品分类主界面</a>";
+}
+else{
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
