0f9a175f0cda4058ce6430e6f961b6250451a750
timoe2020 <64744101+timoe2020@users.noreply.github.com>
2020-07-25 16:36:32 +0800

    Update update.php

--- admin/shopclass/update.php
+++ admin/shopclass/update.php
@@ -1,1 +1,24 @@
 <?php
+include '../../public/common/dbconfig.inc.php';
+$name=$_POST['name'];
+$id=$_POST['id'];
+
+
+
+if($name){
+    $sql="UPDATE  shopclass SET name='$name'
+    WHERE id='$id'";
+//    echo"$sqlUser";
+    if($conn->query($sql))
+    {
+        echo "更新成功!<br>";
+    }
+    else{
+        echo "error:".$sql."<br>".$conn->error;
+    }
+}
+else{
+    echo "名称不能为空";
+}
+echo "<a href='edit.php'>点击返回编辑页面</a>";
+
