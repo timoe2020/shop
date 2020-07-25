558749cbf0937ee77f672484d0c17738479f568a
timoe2020 <64744101+timoe2020@users.noreply.github.com>
2020-07-25 16:38:21 +0800

    Update add.php
    add content

--- admin/user/add.php
+++ admin/user/add.php
@@ -1,1 +1,50 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>添加用户</title>
+</head>
+<body>
+    <p><b>添加用户：</b></p>
+    <form action="insert.php" method="post" target="_self">
+        <table border="1px" cellspacing="0">
+            <tr>
+                <td>用户名：</td>
+                <td>
+                    <label>
+                        <input type="text" name="username">
+                    </label>
+                </td>
+            </tr>
+            <tr>
+                <td>密码：</td>
+                <td>
+                    <label>
+                        <input type="password" name="password">
+                    </label>
+                </td>
+            </tr>
+            <tr>
+                <td>确认密码：</td>
+                <td>
+                    <label>
+                        <input type="password" name="repassword">
+                    </label>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input type="submit" value="提交">
+                    <input type="reset" value="重置">
+                </td>
+            </tr>
+            <tr>
+                <a href="../index.php">返回管理系统主界面</a>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
