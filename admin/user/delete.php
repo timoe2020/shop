21b1533e611328310d81121626fb27f67dc9c039
timoe2020 <64744101+timoe2020@users.noreply.github.com>
2020-07-25 16:38:49 +0800

    Update delete.php

--- admin/user/delete.php
+++ admin/user/delete.php
@@ -1,1 +1,14 @@
 <?php
+    include "../../public/common/dbconfig.inc.php";
+    $id=$_GET['id'];
+    if(mysqli_query($conn,"DELETE FROM userdata WHERE id='{$id}'"))
+    {
+        echo"删除成功<br>";
+        echo"<a href='index.php'>点击返回主界面</a>";
+    }
+    else{
+        echo "something wrong happened";
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+?>
