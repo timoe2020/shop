3d7b0efd3f1fe01f3253c2fb69968ae25302264d
timoe2020 <64744101+timoe2020@users.noreply.github.com>
2020-07-25 16:36:04 +0800

    Update insert.php

--- admin/shopclass/insert.php
+++ admin/shopclass/insert.php
@@ -1,1 +1,19 @@
 <?php
+include '../../public/common/dbconfig.inc.php';
+$name=$_POST['name'];
+
+
+
+$sql="INSERT INTO shopclass (name)
+    VALUES ('$name')";
+
+if($conn->query($sql))
+{
+    echo "添加成功<br>";
+}
+else{
+    echo "error:".$sql."<br>".$conn->error;
+}
+
+
+echo "<a href='add.php'>点击返回商品添加界面</a>";
