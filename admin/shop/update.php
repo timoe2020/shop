1325aa2ba2aff2291f8531b874adb4456244d394
timoe2020 <64744101+timoe2020@users.noreply.github.com>
2020-08-09 11:35:25 +0800

    Update update.php
    add frame

--- admin/shop/update.php
+++ admin/shop/update.php
@@ -2,7 +2,6 @@
 include '../public/common/acl.inc.php';
 include '../../public/common/dbconfig.inc.php';
 header("Content-Type: text/html;charset=utf-8");
-
 ?>
 <!doctype html>
 <html lang="en">
@@ -11,7 +10,7 @@ header("Content-Type: text/html;charset=utf-8");
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>更新商品</title>
+    <title>Document</title>
 </head>
 <body>
 <header>
@@ -22,34 +21,27 @@ header("Content-Type: text/html;charset=utf-8");
 <main>
     <?php
     $name=$_POST['name'];
-    $price=$_POST['price'];
-    $stock=$_POST['stock'];
-    $upshelf=$_POST['upshelf'];
-    $image=$_POST['img'];
-    $brand_id=$_POST['BrandId'];
     $id=$_POST['id'];
 
 
 
     if($name){
-        $sqlShop="UPDATE  shop SET name='$name', price='$price' ,stock='$stock',upshelf='$upshelf'
-    ,image='$image',brand_id='$brand_id' WHERE id='$id'";
-
-        if($conn->query($sqlShop))
+        $sql="UPDATE  shopclass SET name='$name'
+    WHERE id='$id'";
+//    echo"$sqlUser";
+        if($conn->query($sql))
         {
-            echo "更改成功<br>";
+            echo "更新成功!<br>";
         }
         else{
-            echo "error:".$sqlShop."<br>".$conn->error;
+            echo "error:".$sql."<br>".$conn->error;
         }
     }
     else{
         echo "名称不能为空";
     }
-
     ?>
 </main>
-
 </body>
 </html>
 
