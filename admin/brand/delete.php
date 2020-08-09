30f3fb3ab3ff06cee4b4cc1c30e727dbf4e044f4
timoe2020 <64744101+timoe2020@users.noreply.github.com>
2020-08-09 11:10:43 +0800

    Update delete.php
    change the titie of website

--- admin/brand/delete.php
+++ admin/brand/delete.php
@@ -13,7 +13,7 @@ include "../../public/common/dbconfig.inc.php";
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>删除品牌</title>
 </head>
 <body>
 <header>
@@ -38,4 +38,3 @@ include "../../public/common/dbconfig.inc.php";
 
 </body>
 </html>
-
