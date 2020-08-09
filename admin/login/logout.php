aea51f27f5029487c56e4db7b0a6cdf2054d704b
timoe2020 <64744101+timoe2020@users.noreply.github.com>
2020-08-09 11:16:59 +0800

    Update logout.php
    delete some test code

--- admin/login/logout.php
+++ admin/login/logout.php
@@ -1,16 +1,10 @@
 <?php
-echo"success";
-?>
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <a href="login.html">登录</a>
-</body>
-</html>
+header("Content-Type: text/html;charset=utf-8");
+session_start();
+session_unset();
+session_destroy();
+setcookie(session_name(),'',time()-3600,'/');
+echo "<a href='/shop/admin/login/login.html'>登录</a>";
+
+
+
