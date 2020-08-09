a5a96cdf4ba7f58563a8fcd4c98c4b2ab6916c07
timoe2020 <64744101+timoe2020@users.noreply.github.com>
2020-08-09 12:04:59 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,4 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "password";
-$dbname = "shop";
-
-// 创建连接
-$conn = new mysqli($servername, $username, $password, $dbname);
-// 检测连接
-if ($conn->connect_error) {
-    die("连接失败: " . $conn->connect_error);
-}
-
-
-
-
-
-
+header('location:home/index.php');
 //?>
+
