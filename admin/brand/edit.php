4288ae618b09c184ec40c6368f87f1fab9f9b4f1
timoe2020 <64744101+timoe2020@users.noreply.github.com>
2020-08-09 11:09:16 +0800

    Update edit.php
    change content

--- admin/brand/edit.php
+++ admin/brand/edit.php
@@ -2,8 +2,16 @@
 header("Content-Type: text/html;charset=utf-8");
 include '../public/common/acl.inc.php';
 include "../../public/common/dbconfig.inc.php";
+$sqlClass=' SELECT * FROM shopclass';
+$classRes = $conn->query($sqlClass);
+
+//echo $_GET['id'];
+$id=$_GET['id'];
 
 
+$sqlBrand = "SELECT * FROM brand WHERE id='$id'";
+$result = $conn->query($sqlBrand);
+$row = mysqli_fetch_array($result);
 
 
 ?>
@@ -14,7 +22,7 @@ include "../../public/common/dbconfig.inc.php";
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>添加品牌</title>
+    <title>修改品牌</title>
 </head>
 <body>
 <header>
@@ -23,22 +31,34 @@ include "../../public/common/dbconfig.inc.php";
     ?>
 </header>
 <main>
-<?php
-$classId=$_POST['classId'];
-$name=$_POST['name'];
-$sql = "INSERT INTO brand (name,shopclass_id)
-VALUES ('$name', '$classId')";
+<p><b>修改品牌：</b></p>
+<form action="update.php" method="post" target="_self">
+    <span>品牌：</span>
+    <input type="text" name="name" value="<?php echo $row['name']?>">
+    <br>
+    <span>分类：</span>
+    <select name="classId" id="">
+        <option value="">--选择分类--</option>
+        <?php
+        if ($classRes->num_rows > 0) {
+            // 输出数据
+            while($row = $classRes->fetch_assoc()) {
+                echo "<option  value='{$row['id']}'>{$row['name']}</option>";
+            }
+        } else {
+            echo "0 结果";
+        }
+        ?>
+    </select>
+    <br>
+    <input type="hidden" name="id" value="<?php echo $id?>">
+    <input type="submit" value="提交">
+    <input type="reset" value="重置">
+</form>
 
-if ($conn->query($sql)) {
-    echo "添加成功";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
-//echo"<br>";
-//echo"<a href='add.php'>添加品牌</a>";
-//echo"<br>";
-//echo"<a href='../index.php'>返回管理系统主界面</a>"
-?>
 </main>
 </body>
 </html>
+
+
+
