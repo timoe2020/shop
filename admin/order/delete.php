2915f70cdfa0238285fedd590e607b0567313ab0
timoe2020 <64744101+timoe2020@users.noreply.github.com>
2020-08-09 11:18:58 +0800

    Update delete.php
    add content

--- admin/order/delete.php
+++ admin/order/delete.php
@@ -1,1 +1,40 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>删除订单</title>
+</head>
+<body>
+<header>
+    <?php
+    include '../index.php';
+    ?>
+</header>
+<main>
+    <?php
+    include '../public/common/acl.inc.php';
+    include "../../public/common/dbconfig.inc.php";
+    header("Content-Type: text/html;charset=utf-8");
+    $id=$_GET['id'];
+    if(mysqli_query($conn,"DELETE FROM orderdata WHERE id='{$id}'"))
+    {
+        echo"删除成功<br>";
+
+    }
+    else{
+        echo "something wrong happened";
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    ?>
+
+</main>
+
+</body>
+</html>
+
+
+
