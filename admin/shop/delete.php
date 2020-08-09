2dd0b86a5dd30fe9cfd82aa6a92c6369f97a2675
timoe2020 <64744101+timoe2020@users.noreply.github.com>
2020-08-09 11:30:26 +0800

    Update delete.php
    Add content

--- admin/shop/delete.php
+++ admin/shop/delete.php
@@ -1,1 +1,39 @@
 <?php
+    include '../public/common/acl.inc.php';
+    include "../../public/common/dbconfig.inc.php";
+header("Content-Type: text/html;charset=utf-8");
+
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>删除商品</title>
+</head>
+<body>
+    <header>
+        <?php
+        include "../index.php";
+        ?>
+    </header>
+    <main>
+        <?php
+        $id=$_GET['id'];
+        if(mysqli_query($conn,"DELETE FROM shop WHERE id='{$id}'"))
+        {
+            echo"删除成功<br>";
+
+        }
+        else{
+            echo "something wrong happened";
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+        ?>
+    </main>
+
+</body>
+</html>
