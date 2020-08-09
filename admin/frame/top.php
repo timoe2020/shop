686685c35fa78603b3333797bfbe4a6c0bfb805e
timoe2020 <64744101+timoe2020@users.noreply.github.com>
2020-08-09 11:13:48 +0800

    Update top.php
    Add content

--- admin/frame/top.php
+++ admin/frame/top.php
@@ -1,1 +1,22 @@
 <?php
+include '../public/common/acl.inc.php';
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>购物后台系统</title>
+    <link rel="stylesheet" type="text/css" href="/shop/admin/public/css/reset.css">
+    <link rel="stylesheet" type="text/css" href="/shop/admin/public/css/style.css">
+</head>
+<body>
+<div id="header">
+    <div id="logo"></div>
+    <div id="title">购物后台管理系统</div>
+    <div id="time"></div>
+</div>
+</body>
+</html>
