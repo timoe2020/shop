376c0ba203a0ecbf0785f65bc078e1c37d6e807a
timoe2020 <64744101+timoe2020@users.noreply.github.com>
2020-08-09 12:00:55 +0800

    Update insert.php
    add frame

--- admin/user/insert.php
+++ admin/user/insert.php
@@ -1,22 +1,48 @@
 <?php
+include '../public/common/acl.inc.php';
 include '../../public/common/dbconfig.inc.php';
-$username=$_POST['username'];
-$password=$_POST['password'];
-$repassword=$_POST['repassword'];
-$regtime=time();
+header("Content-Type: text/html;charset=utf-8");
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<header>
+    <?php
+        include '../index.php';
+    ?>
+</header>
+<main>
+    <?php
+    $username=$_POST['username'];
+    $password=$_POST['password'];
+    $repassword=$_POST['repassword'];
+    $regtime=time();
 
-if($password===$repassword){
-    $sqlUser="INSERT INTO userdata (username, password,regtime,admin)
+    if($password===$repassword){
+        $sqlUser="INSERT INTO userdata (username, password,regtime,admin)
 VALUES ('$username', '$password','$regtime','0')";
-    if($conn->query($sqlUser))
-    {
-        echo "添加成功!<br>";
+        if($conn->query($sqlUser))
+        {
+            echo "添加成功!<br>";
+        }
+        else{
+            echo "error:".$sqlUser."<br>".$conn->error;
+        }
     }
     else{
-        echo "error:".$sqlUser."<br>".$conn->error;
+        echo "Two password entries are inconsistent";
     }
-}
-else{
-    echo "Two password entries are inconsistent";
-}
-echo "<a href='add.php'>点击返回添加界面</a>";
+
+    ?>
+
+</main>
+
+</body>
+</html>
