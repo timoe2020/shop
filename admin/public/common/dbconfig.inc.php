c335701a358dee1e83eb77cc0d7bbb205b6dca62
timoe2020 <64744101+timoe2020@users.noreply.github.com>
2020-08-09 11:26:20 +0800

    Update dbconfig.inc.php
    delete the password.

--- admin/public/common/dbconfig.inc.php
+++ admin/public/common/dbconfig.inc.php
@@ -1,1 +1,18 @@
 <?php
+
+header("Content-Type: text/html;charset=utf-8");
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "shop";
+
+// 创建连接
+$conn = new mysqli($servername, $username, $password, $dbname);
+// 检测连接
+if ($conn->connect_error) {
+    die("连接失败: " . $conn->connect_error);
+}
+if(!mysqli_set_charset($conn,"utf8")){
+    echo"Error loading character set utf8: %s\n",mysqli_error($conn);
+}
+
