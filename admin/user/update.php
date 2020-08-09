5eb68de380f4230a54297240ba54a6f70b5b67df
timoe2020 <64744101+timoe2020@users.noreply.github.com>
2020-08-09 12:00:26 +0800

    Update update.php
    add frame

--- admin/user/update.php
+++ admin/user/update.php
@@ -1,25 +1,54 @@
 <?php
+include '../public/common/acl.inc.php';
 include '../../public/common/dbconfig.inc.php';
-$username=$_POST['username'];
-$password=$_POST['password'];
-$repassword=$_POST['repassword'];
-$id=$_POST['id'];
-$admin=$_POST['admin'];
+header("Content-Type: text/html;charset=utf-8");
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <?php
+            include '../index.php';
+        ?>
+
+    </header>
+    <main>
+        <?php
+        $username=$_POST['username'];
+        $password=$_POST['password'];
+        $repassword=$_POST['repassword'];
+        $id=$_POST['id'];
+        $admin=$_POST['admin'];
 
 
-if($password===$repassword){
-    $sqlUser="UPDATE  userdata SET username='$username', password='$password'
+        if($password===$repassword){
+            $sqlUser="UPDATE  userdata SET username='$username', password='$password'
     WHERE id='$id'";
 
-    if($conn->query($sqlUser))
-    {
-        echo "更改成功<br>";
-    }
-    else{
-        echo "error:".$sqlUser."<br>".$conn->error;
-    }
-}
-else{
-    echo "两次输入密码不一致";
-}
-echo "<a href='edit.php'>点击返回修改页面</a>";
+            if($conn->query($sqlUser))
+            {
+                echo "更改成功<br>";
+            }
+            else{
+                echo "error:".$sqlUser."<br>".$conn->error;
+            }
+        }
+        else{
+            echo "两次输入密码不一致";
+        }
+
+        ?>
+
+    </main>
+
+</body>
+</html>
+
+
