497893172e80f78037a1a647cb4fc8ad84d60339
timoe2020 <64744101+timoe2020@users.noreply.github.com>
2020-08-09 11:09:43 +0800

    Update delete.php
    Add content

--- admin/brand/delete.php
+++ admin/brand/delete.php
@@ -1,1 +1,41 @@
 <?php
+header("Content-Type: text/html;charset=utf-8");
+include '../public/common/acl.inc.php';
+include "../../public/common/dbconfig.inc.php";
+
+
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<header>
+    <?php
+    include "../index.php";
+    ?>
+</header>
+<main>
+    <?php
+    $id=$_GET['id'];
+    if(mysqli_query($conn,"DELETE FROM brand WHERE id='$id'"))
+    {
+        echo"删除成功!";
+
+    }
+    else{
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+    ?>
+
+</main>
+
+</body>
+</html>
+
