452deaf466eeec35720e16e84d9a3d60ecdf9c62
timoe2020 <64744101+timoe2020@users.noreply.github.com>
2020-08-09 11:33:34 +0800

    Update delete.php
    add frame

--- admin/shop/delete.php
+++ admin/shop/delete.php
@@ -1,9 +1,7 @@
 <?php
-    include '../public/common/acl.inc.php';
-    include "../../public/common/dbconfig.inc.php";
+include '../public/common/acl.inc.php';
+include'../../public/common/dbconfig.inc.php';
 header("Content-Type: text/html;charset=utf-8");
-
-
 ?>
 <!doctype html>
 <html lang="en">
@@ -12,24 +10,24 @@ header("Content-Type: text/html;charset=utf-8");
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>删除商品</title>
+    <title>Document</title>
 </head>
 <body>
     <header>
         <?php
-        include "../index.php";
+            include'../index.php';
         ?>
     </header>
     <main>
         <?php
         $id=$_GET['id'];
-        if(mysqli_query($conn,"DELETE FROM shop WHERE id='{$id}'"))
+        $sql="DELETE FROM shopclass WHERE id='{$id}'";
+        if($conn->query($sql))
         {
-            echo"删除成功<br>";
+            echo "删除成功！";
 
         }
         else{
-            echo "something wrong happened";
             echo "Error: " . $sql . "<br>" . $conn->error;
         }
         ?>
@@ -37,3 +35,6 @@ header("Content-Type: text/html;charset=utf-8");
 
 </body>
 </html>
+
+
+
