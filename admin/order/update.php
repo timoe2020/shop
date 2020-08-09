2dad410f13f59e8472430e9ebb9f346764ffde1a
timoe2020 <64744101+timoe2020@users.noreply.github.com>
2020-08-09 11:20:31 +0800

    Update update.php
    Add content

--- admin/order/update.php
+++ admin/order/update.php
@@ -1,1 +1,25 @@
+
 <?php
+include '../public/common/acl.inc.php';
+include '../../public/common/dbconfig.inc.php';
+header("Content-Type: text/html;charset=utf-8");
+$state=$_POST['state'];
+$id=$_POST['id'];
+
+
+if($state){
+    $sqlStat="UPDATE  orderdata SET orderstat_id='$state' WHERE id='$id'";
+
+
+    if($conn->query($sqlStat))
+    {
+        echo "更改成功<br>";
+    }
+    else{
+        echo "error:".$sqlStat."<br>".$conn->error;
+    }
+}
+else{
+    echo "请选择状态";
+}
+echo "<a href='EditInfo.php'>点击返回修改页面</a>";
