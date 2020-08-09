4e477301ba8798c45b62ee9a7e58eb0819a0e06e
timoe2020 <64744101+timoe2020@users.noreply.github.com>
2020-08-09 11:56:16 +0800

    Update delete.php
    add content

--- admin/shopclass/delete.php
+++ admin/shopclass/delete.php
@@ -1,13 +1,40 @@
 <?php
+include '../public/common/acl.inc.php';
 include'../../public/common/dbconfig.inc.php';
-$id=$_GET['id'];
-$sql="DELETE FROM shopclass WHERE id='{$id}'";
-if($conn->query($sql))
-{
-    echo "删除成功！";
-    echo"<a href='index.php'>点击返回商品分类主界面</a>";
-}
-else{
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
+header("Content-Type: text/html;charset=utf-8");
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <?php
+            include'../index.php';
+        ?>
+    </header>
+    <main>
+        <?php
+        $id=$_GET['id'];
+        $sql="DELETE FROM shopclass WHERE id='{$id}'";
+        if($conn->query($sql))
+        {
+            echo "删除成功！";
+
+        }
+        else{
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+        ?>
+    </main>
+
+</body>
+</html>
+
+
 
