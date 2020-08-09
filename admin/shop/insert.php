0bcf0ed02b131ca320535bfd7c0c7464a4775781
timoe2020 <64744101+timoe2020@users.noreply.github.com>
2020-08-09 11:43:29 +0800

    Update insert.php
    change content

--- admin/shop/insert.php
+++ admin/shop/insert.php
@@ -15,6 +15,53 @@ header("Content-Type: text/html;charset=utf-8");
 <body>
 <header>
     <?php
+    include'../index.php';
+    ?>
+</header>
+<main>
+    <?php
+    $name=$_POST['name'];
+    $price=$_POST['price'];
+    $stock=$_POST['stock'];
+    $upshelf=$_POST['upshelf'];
+    $image=$_POST['img'];
+    $brand_id=$_POST['BrandId'];
+    $class_id=$_POST['ClassId'];
+
+
+
+    $sqlShop="INSERT INTO shop (name, price, stock, upshelf, image, brand_id,class_id)
+VALUES ('$name', '$price',' $stock', '$upshelf',' $image', '$brand_id','$class_id')";
+    if($conn->query($sqlShop))
+    {
+        echo "添加成功!<br>";
+    }
+    else{
+        echo "error:".$sqlShop."<br>".$conn->error;
+    }
+
+
+
+    ?>
+</main>
+</body>
+</html>
+include '../public/common/acl.inc.php';
+include '../../public/common/dbconfig.inc.php';
+header("Content-Type: text/html;charset=utf-8");
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<header>
+    <?php
     include '../index.php';
     ?>
 </header>
